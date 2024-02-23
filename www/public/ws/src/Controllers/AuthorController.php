1be62ae0947984933db9abd67bf1ea72b8383112
oscar <oscar@localhost.localdomain>
2024-02-22 22:20:52 -0800

    Refractor: reEnd-Point regristrar autor

--- www/public/ws/src/Controllers/AuthorController.php
+++ www/public/ws/src/Controllers/AuthorController.php
@@ -10,21 +10,42 @@ class AuthorController {
 
     public function create_author(Request $request, Response $response, $args) {
         $body = $request->getParsedBody();
+        $http_code = 200;
         
         if(empty($body) || !is_array($body)){
             $response->getBody()->write(json_encode(['error' => 'Cuerpo JSON no válido']));
             return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
         }
         
-        $author = $body['author'];
+        $result_array = array();
+        $author = trim(ucwords(strtolower($body['author'])));
+        $author = preg_replace('/\s+/', ' ', $author);
+        
+        $id_author = Author::insertAuthor($author);
 
-        $result_array = [
-            "status" => true,
-            "id_autor" => Author::insertAuthor($author)
-        ];
+        if (is_null($id_author)) {
+            $http_code = 409; // Conflict
+            $result_array = array(
+                "error" => array(
+                    "code" => $http_code,
+                    "mensaje" => "Autor existente",
+                    "descripcion" => "El Autor ya se encuentra registrado"
+                )
+            );
+        } else {
+            $http_code = 201;
+            $result_array = array(
+                "resultado" => array(
+                    "code" => $http_code,
+                    "mensaje" => "Autor registrado",
+                    "id_autor" => $id_author
+                )
+            );
+        }
+
 
         $result_json = json_encode($result_array, JSON_UNESCAPED_UNICODE);
         $response->getBody()->write($result_json);
-        return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
+        return $response->withHeader('Content-Type', 'application/json')->withStatus($http_code);
     }
 }
--- www/public/ws/src/Models/AuthorModel.php
+++ www/public/ws/src/Models/AuthorModel.php
@@ -6,10 +6,13 @@ use App\Models\SqlModel;
 class AuthorModel extends SqlModel{
 
     public static function insertAuthor($author) {
-        $query_author = "INSERT INTO author(name)VALUES(:author) RETURNING id;";
+        $query_author = "INSERT INTO author(name)VALUES(:author) ON CONFLICT(name) DO NOTHING RETURNING id;";
         $params_insert = [":author" => $author];
         $result = self::executeInsertAuthor($query_author, $params_insert);
 
-        return $result[0]['id'];
+        file_put_contents("php://stdout", " CONSOLE: [result]: " . print_r($result, true));
+        
+
+        return isset($result[0]['id']) ? $result[0]['id'] : null;
     }
 }
